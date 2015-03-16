0637c31335bf2572bfd02d6858ac1f4d76003f7d
Ville Korhonen <ville@xd.fi>
2015-03-16 18:41:44 +0200

    Add script for generating directory index listings.

---
+++ dirlist.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+TEMPLATE = """
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>Directory listing for %(dir)s</title>
+    <style>
+        body {
+            font-family: Helvetica, Arial, sans-serif;
+        }
+        .dir a {
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+<h1>Directory listing for %(dir)s</h1>
+<ul>
+%(content)s
+</ul>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-420043-36', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
+"""
+
+
+def main(args):
+    path = args.path
+    if not os.path.exists(path):
+        print "Path not found: %s" % path
+        return 1
+    
+    for root, dirs, files in os.walk(path):
+        outfile = '%s/index.html' % root
+        content = ""
+        for d in dirs:
+            content += '<li class="dir"><a href="%s">%s</a></li>' % (d, d)
+        for f in files:
+            content += '<li class="file"><a href="%s">%s</a></li>' % (f, f)
+        
+        src = TEMPLATE % {
+            'dir': os.path.basename(root),
+            'content': content,
+        }
+        with open(outfile, 'w') as ofile:
+            ofile.write(src)
+        print "Wrote dir %s to file %s." % (root, outfile)
+
+    return 0
+
+def run():
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('path')
+    args = parser.parse_args()
+    sys.exit(main(args))
+
+if __name__ == "__main__":
+    run()
